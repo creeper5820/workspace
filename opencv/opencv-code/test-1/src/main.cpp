9331c2ae37917ae8fc12b081d37811e30a8dde25
creeper5820 <cqq1960180796@163.com>
2024-03-03 12:01:27 +0800

    2233

--- opencv/opencv-code/test-1/src/main.cpp
+++ opencv/opencv-code/test-1/src/main.cpp
@@ -1,9 +1,27 @@
 #include "lucas-kanade.hpp"
 
-int main(int argc, char** argv)
-{
-    std::string path;
-    path = "/workspaces/codespace/opencv/opencv-code/test-1/video/test-2.mp4";
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
 
-    lucas_kanade(path);
+// int main(int argc, char** argv)
+// {
+//     std::string path;
+//     path = "/workspaces/codespace/opencv/opencv-code/test-1/video/test-2.mp4";
+
+//     lucas_kanade(path);
+// }
+
+int main()
+{
+    auto capture = cv::VideoCapture();
+
+    try {
+        capture.open("22");
+        capture.isOpened();
+
+    } catch (cv::Exception& e) {
+
+        const char* error = e.what();
+        std::cout << "excepthon: " << error << std::endl;
+    }
 }
