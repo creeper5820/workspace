a19d944991f90902cc957f6886e6bbd26d85a40b
creeper5820 <cqq1960180796@163.com>
2024-03-09 14:25:29 +0800

    pcl develop start

--- auto-alliance/project/cloud-viewer/src/main.cpp
+++ auto-alliance/project/cloud-viewer/src/main.cpp
@@ -1,1 +1,32 @@
-#include
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+#include <boost/smart_ptr/shared_ptr.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+int main()
+{
+    using PointType = pcl::PointXYZ;
+
+    auto box = pcl::PointCloud<PointType>::Ptr(new pcl::PointCloud<PointType>());
+
+    for (int x = 0; x < 100; x++)
+        for (int y = 0; y < 100; y++)
+            for (int z = 0; z < 100; z++) {
+            }
+
+    auto view = boost::shared_ptr<pcl::visualization::PCLVisualizer>();
+    view->setBackgroundColor(0, 0, 0);
+    view->addPointCloud<PointType>(box, "box");
+
+    view->spin();
+
+    return 0;
+}
--- code/codes/test/src/main.cpp
+++ code/codes/test/src/main.cpp
@@ -1,32 +1,71 @@
 #include <initializer_list>
+
+#include <functional>
 #include <iostream>
+#include <memory>
+#include <type_traits>
 
 template <typename T>
-T add(T f)
+class ITask {
+public:
+    using HandleType = std::function<void(T)>;
+
+    ITask(HandleType callback)
+    {
+        callback_ = callback;
+    }
+
+    ITask() { }
+
+    void set_handle(HandleType callback)
+    {
+        callback_ = callback;
+    }
+
+    void callback(T a)
+    {
+        callback_(a);
+    }
+
+private:
+    HandleType callback_;
+};
+
+class Big { };
+
+void print_error(float)
 {
-    return f;
+    std::cout << "error" << '\n';
 }
 
-template <typename T, typename... Args>
-T add(T first, Args... args)
+template <typename T>
+void print_number(T num)
 {
-    return first + add<T>(args...);
+    std::cout << num << '\n';
 }
 
-template <class F, class... Args>
-void expand(const F& f, Args&&... args)
+void handle(const Big&& big)
 {
-    auto a = std::initializer_list<int> {
-        ((f(std::forward<Args>(args)), 0))...
-    };
 }
 
 int main()
 {
-    expand([](int i) -> void {
-        std::cout << i << std::endl;
-    },
-        1, 2, 3);
+    // initialize
+    auto task_1_ptr = std::make_shared<ITask<int>>();
+    task_1_ptr->set_handle([](int) -> void {
+        std::cout << "error" << '\n';
+    });
+
+    auto task_2_ptr = std::make_shared<ITask<float>>();
+    task_2_ptr->set_handle(print_error);
+
+    ////////
+    // task_1_ptr->callback();
+    // task_2_ptr->callback();
+
+    print_number(2.3);
+    print_number(std::string("2233"));
+    ///////
 
     return 0;
 }
